87a21b261834c7f0a42021a6fd30259a456365a0
xaro <roberto.poo@gmail.com>
2022-09-24 17:36:51 -0400

    Add resiliant method to get all info

--- fujitsu/fujitsu.py
+++ fujitsu/fujitsu.py
@@ -1,32 +1,99 @@
-from datetime import datetime
+import datetime, random, time, requests
 from urllib.parse import urljoin
-import requests
+from fujitsu.hvac_info import HvacInfo
+
 
 HEADERS = {
-  'X-Requested-With': 'XMLHttpRequest',
-  'Content-Type': 'application/x-www-form-urlencoded',
+    "X-Requested-With": "XMLHttpRequest",
+    "Content-Type": "application/x-www-form-urlencoded",
 }
+
+SESSION_ERROR = "-13"
+
+# Disable warnings as there will always be a self signed cert
+requests.packages.urllib3.disable_warnings(
+    requests.packages.urllib3.exceptions.InsecureRequestWarning
+)
+
+
+def retry_with_backoff(retries=5, backoff_in_seconds=1):
+    def rwb(f):
+        def wrapper(*args, **kwargs):
+            x = 0
+            while True:
+                try:
+                    return f(*args, **kwargs)
+                except:
+                    if x == retries:
+                        raise
+
+                    sleep = backoff_in_seconds * 2**x + random.uniform(0, 1)
+                    time.sleep(sleep)
+                    x += 1
+
+        return wrapper
+
+    return rwb
 
 
 class FujitsuHvac:
-    def __init__(self, base_url: str) -> None:
+    def __init__(self, base_url: str, username: str, password: str) -> None:
         self.base_url = base_url
+        self.username = username
+        self.password = password
+        self.session = None
 
-    def login(self, username: str, password: str) -> requests.Session:
+    def login(self):
+        # Ensure that we are not logged in before, if not will get
+        # error 4
+        self.logout()
+
         # TODO: Handle failures like bad user/pass
         payload = {
-            "username": username,
-            "password": password,
-            "logintime": datetime.now().isoformat()
+            "username": self.username,
+            "password": self.password,
+            "logintime": (datetime.datetime.now()).isoformat(),
         }
 
         session = requests.Session()
 
-        session.post(
-            urljoin(self.base_url, "login.cgi"),
-            data=payload,
-            verify=False,
-            headers=HEADERS
+        response = session.post(
+            self.url("login.cgi"), data=payload, verify=False, headers=HEADERS
         )
 
-        return session
+        if response.text != "0":
+            print("Error logging in: " + response.text)
+            raise Exception("Error response " + response.text)
+
+        self.session = session
+
+    def logout(self):
+        requests.post(self.url("logout.cgi"), verify=False)
+        self.session = None
+
+    @retry_with_backoff(retries=5)
+    def get_all_info(self) -> list[HvacInfo]:
+        if self.session is None:
+            self.login()
+
+        response = self.session.post(
+            self.url("getmondata.cgi"),
+            data={"FunctionNo": 2, "Argument1": -1},
+            headers=HEADERS,
+            verify=False,
+        ).text
+
+        if response == SESSION_ERROR:
+            self.logout()
+            print("Session Error -13 found")
+            raise Exception("Session error found")
+
+        infos = []
+        for info in response.split("\n"):
+            if len(info.strip()) == 0:
+                break
+            infos.append(HvacInfo.from_info(info.split(",")))
+        return infos
+
+    def url(self, path: str) -> str:
+        return urljoin(self.base_url, path)
--- tests/test_fujitsu.py
+++ tests/test_fujitsu.py
@@ -1,5 +1,5 @@
-from urllib import response
 from fujitsu.fujitsu import FujitsuHvac
+from fujitsu.hvac_info import FanSpeed, HvacInfo, Mode
 import responses
 from urllib.parse import urljoin
 
@@ -8,16 +8,78 @@ BASE_URL = "https://baseurl.com"
 
 @responses.activate
 def test_login():
-    responses.add(responses.POST,
-                  urljoin(BASE_URL, "login.cgi"),
-                  adding_headers={
-                      "set-cookie": "sessionid=cookie; " +
-                      "path=/; " +
-                      ""
-                  })
+    mock_login("0", "cookie")
+    mock_logout()
 
-    hvac = FujitsuHvac(BASE_URL)
+    hvac = FujitsuHvac(BASE_URL, "user", "pass")
 
-    session = hvac.login("user", "password")
+    hvac.login()
 
-    assert session.cookies.get('sessionid') == 'cookie'
+    assert hvac.session.cookies.get("sessionid") == "cookie"
+
+
+@responses.activate
+def test_get_all_info():
+    mock_login("0", "cookie")
+    mock_logout()
+    mock_getmondata(
+        """2,514,01-01,0,0,0,0,0,0,0,0,0,0,0,0,0,1,240,0,0,7,8,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-2,-2,0
+2,515,01-02,0,0,0,0,0,0,0,0,0,0,0,0,0,1,230,0,0,7,8,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-2,-2,0
+2,516,02-01,0,0,0,0,0,0,0,0,0,0,0,0,0,1,230,0,0,7,8,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-2,-2,0"""
+    )
+
+    hvac = FujitsuHvac(BASE_URL, "user", "pass")
+
+    info = hvac.get_all_info()
+
+    assert info == [
+        HvacInfo(1, 1, False, Mode.COOL, 24.0, FanSpeed.OFF, 7),
+        HvacInfo(1, 2, False, Mode.COOL, 23.0, FanSpeed.OFF, 7),
+        HvacInfo(2, 1, False, Mode.COOL, 23.0, FanSpeed.OFF, 7),
+    ]
+
+
+@responses.activate
+def test_get_all_info_retries():
+    mock_login("0", "cookie")
+    mock_logout()
+    # Add one error that should be retried
+    mock_getmondata("-13")
+    mock_getmondata(
+        "2,514,01-01,0,0,0,0,0,0,0,0,0,0,0,0,0,1,240,0,0,7,8,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-2,-2,0"
+    )
+
+    hvac = FujitsuHvac(BASE_URL, "user", "pass")
+
+    info = hvac.get_all_info()
+
+    assert info == [
+        HvacInfo(1, 1, False, Mode.COOL, 24.0, FanSpeed.OFF, 7),
+    ]
+
+
+def mock_login(body: str, session_id: str):
+    responses.add(
+        responses.POST,
+        urljoin(BASE_URL, "login.cgi"),
+        adding_headers={
+            "set-cookie": "sessionid=" + session_id + "; " + "path=/; " + ""
+        },
+        body=body,
+    )
+
+
+def mock_logout():
+    responses.add(
+        responses.POST,
+        urljoin(BASE_URL, "logout.cgi"),
+        adding_headers={"set-cookie": "sessionid=; " + "path=/; " + ""},
+    )
+
+
+def mock_getmondata(body: str):
+    responses.add(
+        responses.POST,
+        urljoin(BASE_URL, "getmondata.cgi"),
+        body=body,
+    )
