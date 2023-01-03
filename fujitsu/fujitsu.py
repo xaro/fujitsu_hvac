1d3ad72a30aa3fe32afe1f715272658e328b995c
xaro <roberto.poo@gmail.com>
2023-01-02 20:15:15 -0500

    Async rewrite

--- fujitsu/fujitsu.py
+++ fujitsu/fujitsu.py
@@ -1,4 +1,4 @@
-import datetime, random, time, requests, asyncio
+import datetime, random, aiohttp, asyncio, logging
 from urllib.parse import urljoin
 from fujitsu.hvac_info import HvacInfo, Mode, FanSpeed
 
@@ -10,39 +10,26 @@ HEADERS = {
 
 SESSION_ERROR = "-13"
 
-# Disable warnings as there will always be a self signed cert
-requests.packages.urllib3.disable_warnings(
-    requests.packages.urllib3.exceptions.InsecureRequestWarning
-)
-
-
-def retry_with_backoff(retries=5, backoff_in_seconds=1):
-    def rwb(f):
-        def wrapper(*args, **kwargs):
-            x = 0
-            while True:
-                try:
-                    return f(*args, **kwargs)
-                except:
-                    if x == retries:
-                        raise
-
-                    sleep = backoff_in_seconds * 2**x + random.uniform(0, 1)
-                    time.sleep(sleep)
-                    x += 1
-
-        return wrapper
-
-    return rwb
-
+async def on_request_start(session, context, params):
+    logging.getLogger('aiohttp.client').debug(f'Starting request <{params}>')
+    logging.getLogger('aiohttp.client').debug(f'Starting with <{session}>')
 
 class FujitsuHvac:
     def __init__(self, base_url: str, username: str, password: str) -> None:
+        logging.basicConfig(level=logging.DEBUG)
+        trace_config = aiohttp.TraceConfig()
+        trace_config.on_request_start.append(on_request_start)
+        self.session = aiohttp.ClientSession(
+            connector=aiohttp.TCPConnector(ssl=False),
+            trace_configs=[trace_config],
+            cookie_jar=aiohttp.CookieJar(unsafe=True)
+        )
+
         self.base_url = base_url
         self.username = username
         self.password = password
-        self.session = None
 
+    # Returns cookies
     async def login(self):
         # Ensure that we are not logged in before, if not will get
         # error 4
@@ -55,45 +42,37 @@ class FujitsuHvac:
             "logintime": (datetime.datetime.now()).isoformat(),
         }
 
-        session = requests.Session()
+        async with self.session.post(
+            self.url("login.cgi"), data=payload, headers=HEADERS
+        ) as response:
+            response_body = await response.text()
+            if response_body != "0":
+                raise Exception("Error response " + response_body)
 
-        response = session.post(
-            self.url("login.cgi"), data=payload, verify=False, headers=HEADERS
-        )
+    async def logout(self):    
+        async with self.session.post(self.url("logout.cgi")):
+            self.session.cookie_jar.clear()
+            pass
 
-        if response.text != "0":
-            print("Error logging in: " + response.text)
-            raise Exception("Error response " + response.text)
-
-        self.session = session
-
-    async def logout(self):
-        requests.post(self.url("logout.cgi"), verify=False)
-        self.session = None
-
-    @retry_with_backoff(retries=5)
-    async def get_all_info(self) -> list[HvacInfo]:
-        if self.session is None:
-            await self.login()
-
-        response = self.session.post(
+    async def get_all_info(self):
+        async with self.session.post(
             self.url("getmondata.cgi"),
             data={"FunctionNo": 2, "Argument1": -1},
-            headers=HEADERS,
-            verify=False,
-        ).text
+            headers=HEADERS
+        ) as response:
+            body = await response.text()
 
-        if response == SESSION_ERROR:
-            self.logout()
-            print("Session Error -13 found")
-            raise Exception("Session error found")
+            if body == SESSION_ERROR:
+                print("Session Error -13 found")
+                # self.cookies = None
+                raise Exception("Session error found")
 
-        infos = []
-        for info in response.split("\n"):
-            if len(info.strip()) == 0:
-                break
-            infos.append(HvacInfo.from_info(info.split(",")))
-        return infos
+            infos = []
+            for info in body.split("\n"):
+                if len(info.strip()) == 0:
+                    break
+                infos.append(HvacInfo.from_info(info.split(",")))
+            return infos
 
     # @retry_with_backoff(retries=3)
     async def set_settings(
@@ -105,15 +84,12 @@ class FujitsuHvac:
         new_fan_speed: FanSpeed = None,
         new_temp: float = None,
     ):
-        if self.session is None:
-            await self.login()
-
         headers = {
             "X-Requested-With": "XMLHttpRequest",
             "Content-Type": "application/json",
         }
 
-        response = self.session.post(
+        async with self.session.post(
             self.url("command.cgi"),
             data={
                 "arg1": 0,
@@ -121,14 +97,15 @@ class FujitsuHvac:
                     circuit, sub_id, new_power_status, new_mode, new_fan_speed, new_temp
                 ),
             },
-            headers=headers,
-            verify=False,
-        ).text
-
-        if response != "0":
-            self.logout()
-            print("Error: " + response)
-            raise Exception("Error found")
+            headers=headers
+        ) as response:
+            await self.logout()
+            
+            response_body = response.text()
+            if response_body != "0":
+                self.logout()
+                print("Error: " + response_body)
+                raise Exception("Error found")
 
     def url(self, path: str) -> str:
         return urljoin(self.base_url, path)
